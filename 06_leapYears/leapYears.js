c7f38e0afb4be693abe493145d4b5e60cf08937b
unprobable-x <bryant.graham@gmail.com>
2022-05-27 15:39:15 -0400

    all passed!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    result = false;
+    if (year % 400 == 0 ) {
+        result = true;
+    }
+    else if (year % 4 == 0 && year % 100 != 0) {
+        result = true;
+    }
+    else {
+        result = false;
+    }
+    return result;
 };
 
 // Do not edit below this line
