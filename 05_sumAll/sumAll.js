e34621124f4616c0639c3d6de55866c171ce139e
unprobable-x <bryant.graham@gmail.com>
2022-05-27 15:21:55 -0400

    boom shakka lakka

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let a = Math.min(x, y);
+    let b = Math.max(x,y);
+    let finalSum = a;
+    if (x < 0 || y < 0) {
+        finalSum = 'ERROR';
+    }
+    else if (Number.isInteger(x) == false || Number.isInteger(y) == false) {
+        finalSum = 'ERROR';
+    }
+    else {
+        for (i = a+1; i <= b; i++) {
+                finalSum += i;
+            }
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
