f31d37a59d94266cb6b4cbef879af0d50e7dd8ab
unprobable-x <bryant.graham@gmail.com>
2022-05-27 15:51:00 -0400

    yippy ky yay!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let num = (temp - 32) * (5/9);
+  return parseFloat(num.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let num = (temp * (9/5)) + 32;
+  return parseFloat(num.toFixed(1));
 };
 
 // Do not edit below this line
