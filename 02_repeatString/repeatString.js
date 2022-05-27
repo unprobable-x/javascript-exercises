2daaee02d94d3ba3dde30b777785ce27784a03a5
unprobable-x <bryant.graham@gmail.com>
2022-05-27 14:42:10 -0400

    solved!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = '';
+    if (num < 0) {
+        output = 'ERROR';
+    }
+    else {
+        for (let i = 0; i < num; i++) {
+            output = output + string;
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
