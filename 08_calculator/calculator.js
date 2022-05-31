552070490de0f21cea1ed5da251f8696c14c35fe
unprobable-x <bryant.graham@gmail.com>
2022-05-31 16:55:03 -0400

    solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,39 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce(add, 0); 
+  function add(accumulator, x) {
+    return accumulator + x;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce(mult, 1); 
+  function mult(accumulator, x) {
+    return accumulator * x;
+  }
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+    var array = [];
+    for (i = 1; i <= x; i++) {
+      array.push(i);
+    }  
+    return array.reduce(function(a, b) {
+      return a * b;
+    },1)
 };
+
 
 // Do not edit below this line
 module.exports = {
