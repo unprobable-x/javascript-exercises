69eb019f7aba5a3052e705aaaf184b485bc9096f
unprobable-x <bryant.graham@gmail.com>
2022-05-31 20:09:34 -0400

    solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    return string.toLowerCase().replace(/\W/g, '').split('').reverse().join('') == 
+    string.toLowerCase().replace(/\W/g, '')
 };
-
 // Do not edit below this line
 module.exports = palindromes;
