04f48b370698fe35406fa2355415c6cb060177a7
unprobable-x <bryant.graham@gmail.com>
2022-05-31 20:28:57 -0400

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    let int = parseInt(x);
+    let result = 0;
+    if (int < 0) {
+        result = "OOPS"
+    } 
+    else if (int == 1) {
+        result = 1;
+    }
+    else {
+        var i;
+        var fib = [0, 1]; 
+        
+        for (i = 2; i <= int; i++) {
+          fib[i] = fib[i - 2] + fib[i - 1];
+         result = fib[int];
+    }
+    }
+    return result;
 };
 
 // Do not edit below this line
