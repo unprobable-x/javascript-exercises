2c333a3cf9c6ae1b45ecc2a594008d5fa09eee3a
unprobable-x <bryant.graham@gmail.com>
2022-05-31 21:24:55 -0400

    first try

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(b) {
+    const age = b.map(y => {if (y.yearOfDeath = '') {
+        y.age = 2022 - y.yearOfBirth;
+        }
+        else {
+            y.age = y.yearOfDeath - y.yearOfBirth
+        }
+    })
+    let total = 0;
+    let oldest = {};
+    b.reduce((prev, curr) => {
+        if ((curr.age) > total) {
+          total = (curr.age);
+          oldest = curr
+        }
+        return prev;
+      })
+    // b.reduce((prev, curr) => {
+    //     if ((curr.yearOfDeath - curr.yearOfBirth) > total) {
+    //       total = (curr.yearOfDeath - curr.yearOfBirth);
+    //       oldest = curr
+    //     }
+    //     return prev;
+    //   })
+      return oldest;
 };
 
 // Do not edit below this line
